df03028a6aa454b375ea8b3ba6a60ca4e8963358
Sekou Dukuly <sdukuly00@gmail.com>
2023-09-28 23:12:57 -0400

    Error resolutions

--- .history/.github/workflows/provider_20230928230633.tf
+++ .history/.github/workflows/provider_20230928230633.tf
@@ -2,11 +2,10 @@ provider "aws" {
   region = "us-east-1" # Replace with your desired AWS region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-053b0d53c279acc90" # Replace with your desired AMI ID
+resource "aws_instance" "sekouEC2" {
+  ami   = "ami-053b0d53c279acc90" # Replace with your desired AMI ID
   instance_type = "t2.micro"
 }
-
 
 resource "aws_vpc" "Sekou-test"{
     cidr = "10.0.0.0/16"
